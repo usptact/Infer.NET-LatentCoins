b7710d8a05837221ecef584f679937ac4e6c727f
Vladislavs Dovgalecs <usptact@inbox.lv>
2018-05-08 14:21:15 -0700

    Add data generation: - complete dataset - incomplete dataset

---
+++ DataGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using MicrosoftResearch.Infer;
+using MicrosoftResearch.Infer.Models;
+using MicrosoftResearch.Infer.Distributions;
+
+namespace LatentCoins
+{
+    public class DataGenerator
+    {
+        public DataGenerator()
+        {
+        }
+
+		public static int[] GenerateLatent(int numExperiments, int numTrials,
+		                                   double selectProb, double biasA, double biasB)
+		{
+			int[] data = new int[numExperiments];
+
+			Bernoulli selector = new Bernoulli(selectProb);
+			Binomial coinA = new Binomial(numTrials, biasA);
+			Binomial coinB = new Binomial(numTrials, biasB);
+
+			for (int i = 0; i < numExperiments; i++)
+			{
+				if (selector.Sample())
+					data[i] = coinA.Sample();
+				else
+					data[i] = coinB.Sample();
+			}
+
+			return data;
+		}
+
+		public static void GenerateComplete(int numExperiments, int numTrials,
+                                            double selectProb, double biasA, double biasB,
+		                                    out int[] data, out int[] assignment)
+		{
+			data = new int[numExperiments];
+			assignment = new int[numExperiments];
+
+            Bernoulli selector = new Bernoulli(selectProb);
+            Binomial coinA = new Binomial(numTrials, biasA);
+            Binomial coinB = new Binomial(numTrials, biasB);
+
+            for (int i = 0; i < numExperiments; i++)
+            {
+                if (selector.Sample())
+				{
+					assignment[i] = 0;
+					data[i] = coinA.Sample();
+				}
+				else
+				{
+					assignment[i] = 1;
+					data[i] = coinB.Sample();
+				}
+            }
+		}
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@ namespace LatentCoins
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+			int numExperiments = 5;
+			int numTrials = 10;
+			double selectProb = 0.5;
+			double biasA = 0.3;
+			double biasB = 0.6;
+
+			int[] data = DataGenerator.GenerateLatent(numExperiments, numTrials,
+													  selectProb, biasA, biasB);
         }
     }
 }
